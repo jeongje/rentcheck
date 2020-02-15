e4c19737c8a321a6663664b880738bff6c89b2ae
jeongje <uonnow@gmial.com>
2020-02-15 12:09:42 +0900

    이름 필터기능

--- excelfile.py
+++ excelfile.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 from openpyxl import load_workbook
 
 EXCEL_FIlE = 'C:\\Users\\82104\\Desktop\\rentcheck\\exfile\\ex.xlsx'
+
 wb = load_workbook(EXCEL_FIlE, data_only=True)
 ws1 = wb.worksheets[0]
 
+
+NAME = '김슬아'
+# print(ws1.rows[0])
 
 all_values = []
 for row in ws1.rows:
@@ -16,17 +20,26 @@ for row in ws1.rows:
     all_values.append(row_value)
 
 
+name_values = []
+for row_values in all_values:
+    if NAME in row_values:
+        name_values.append(row_values)
+
+# print(name_values)
+
+
 @app.route('/')
 def index():
     return render_template(
         'index.html',
         allValues = all_values,
+        nameValues = name_values
     )
 
 @app.route('/check')
 def chech():
     return render_template(
-        'check.thml',
+        'check.html',
     )
 
 
